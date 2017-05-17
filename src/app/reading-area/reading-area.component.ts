2181b6c53a0375badc7f62623a9030b1340739e7
Stephen New <stephenjnew.contact@gmail.com>
2017-05-17 22:10:53 +1000

    Dynamic building of reading area content, stopping before activating scroll bars

--- src/app/reading-area/reading-area.component.ts
+++ src/app/reading-area/reading-area.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
+import { ChangeDetectorRef } from "@angular/core";
+//import { NgZone } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
-import {ViewChild, ViewChildren, QueryList, ElementRef} from '@angular/core'
+import { ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 import { BookLibraryService } from "../book-library.service";
 import { ReaderStateService } from "../reader/reader-state.service";
@@ -15,34 +17,48 @@ export class ReadingAreaComponent implements OnInit {
   chapterContent: string;
   areaScrollHeight = 0;
   areaHeight = 0;
+  chapterContentAsDocument: HTMLDocument;
 
-  @ViewChild('readingAreaContainer') readingAreaContainer:ElementRef; 
-  
+  @ViewChild('readingAreaContainer') readingAreaContainer: ElementRef;
+
   constructor(
     private route: ActivatedRoute,
+    private ref: ChangeDetectorRef,
+    //private zone: NgZone,
     private bookLibraryService: BookLibraryService,
-    private readerStateService: ReaderStateService) {}
+    private readerStateService: ReaderStateService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params =>
       this.readerStateService.emitChapterIndexChange(+params['chapterIndex']));
-    this.route.data.subscribe(data => this.chapterContent = data['chapterContent']);
+    this.route.data.subscribe(data => this.storeAndDisplayChapterContent(data['chapterContent']));
   }
 
-  // areaScrollHeight(): number {
-  //   return this.readingAreaContainer.nativeElement.scrollHeight;
-  // }
-  
-  // areaHeight(): number {
-  //   return this.readingAreaContainer.nativeElement.height;
-  // }
+  private storeAndDisplayChapterContent(chapterContentHtmlAsText: string) {
+    const parser = new DOMParser();
+    this.chapterContentAsDocument = parser.parseFromString(chapterContentHtmlAsText, "text/html");
 
-  onClickMe() {
-    this.areaScrollHeight = this.currentScrollHeight;
-    this.areaHeight = this.currentHeight;
+    const htmlNode = this.chapterContentAsDocument.children[0];
+    const bodyNode = htmlNode.children[1]; // Skip past <html> node
+    this.chapterContent = "";
+
+    for (let index = 0; index < bodyNode.children.length; index++) {
+      const chapterContentWithoutNextParagraph = this.chapterContent;
+      const element = bodyNode.children[index];
+      this.chapterContent += element.outerHTML;
+
+      this.ref.detectChanges();
+
+      if (this.isReadingContainerLargerThanPage()) {
+        this.chapterContent = chapterContentWithoutNextParagraph;
+        break;
+      }
+    }
   }
 
-  get currentScrollHeight(): number { return this.readingAreaContainer.nativeElement.scrollHeight; }
-
-  get currentHeight(): number { return this.readingAreaContainer.nativeElement.clientHeight; }
+  private isReadingContainerLargerThanPage(): boolean {
+    let div = this.readingAreaContainer.nativeElement;
+    let divOffsetBottom = div.offsetTop + div.clientHeight;
+    return divOffsetBottom > document.body.clientHeight;
+  } 
 }
